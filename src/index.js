62aecfbd98498cdf7a8d0f8d8d8adc46ee762f4c
kalnynsh <kda869@yandex.ru>
2020-05-03 19:15:13 +0300

    Refactored with App and Provider components.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './reducer';
-import * as actions from './actions';
-import Counter from './counter';
+import App from './components/app';
 
 const store = createStore(reducer);
-const { dispatch } = store;
 
-// inc, dec, rnd - wrapped functions with dispatch
-const {
-    inc,
-    dec,
-    rnd
-} = bindActionCreators(actions, dispatch);
-
-const update = () => {
-    ReactDOM.render(
-        <Counter
-            counter={store.getState()}
-            doInc={inc}
-            doDec={dec}
-            doRnd={() => {
-                const payload = Math.floor(Math.random() * 10 + 1);
-                rnd(payload);
-            }}
-        />,
-        document.getElementById('root')
-    );
-};
-
-update();
-
-store.subscribe(update);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
